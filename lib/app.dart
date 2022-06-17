60bb53541039f726bf2fcabd92d952f96deca8e6
MuyonZ <pattaraponler@odds.team>
2022-06-17 19:06:57 +0700

    separate flutter stucture

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/screen/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
