50c95128fe35f9716c4faf54fd97b66217e3ebe2
MuyonZ <pattaraponler@odds.team>
2022-06-20 19:00:12 +0700

    Add text field title and desc to add todo screen

--- lib/screens/add_todo.dart
+++ lib/screens/add_todo.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
-class AddTodoScreen extends StatelessWidget {
+class AddTodoScreen extends StatefulWidget {
   static const routeName = "/add-todo";
   const AddTodoScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AddTodoScreen> createState() => _AddTodoScreenState();
+}
+
+class _AddTodoScreenState extends State<AddTodoScreen> {
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Todo"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        child: Column(
+          children: [
+            TextField(controller: titleController),
+            const SizedBox(height: 24),
+            TextField(controller: descriptionController),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              child: Text("Submit"),
+              onPressed: () {
+                print(titleController.text);
+                print(descriptionController.text);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
